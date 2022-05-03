e6492b5816056275ecfaa1b1c88a425b253f632b
Jose Medina Tortosa <josemedinatortosa@gmail.com>
2022-05-03 10:28:36 +0000

    listo para enviar

---
+++ src/js/component/card.js
@@ -0,0 +1,57 @@
+import React, { useState, useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
+import propTypes from "prop-types";
+import { Link } from "react-router-dom";
+
+
+
+export const Card = (props) => {
+  const { store, actions } = useContext(Context);
+
+ 
+
+  return (
+    <div
+      style={{ width: "20rem" }}
+      className=" card  m-2"
+    >
+      <img
+        src= {props.img}
+        className="card-img-top"
+        alt="Image not found" width="400" height="300"
+      />
+      <div className="card-body">
+        <h5 className="card-title">{props.title}</h5>
+        <div className="d-flex"><p className="mx-2"> {props.description1}</p><p> {props.result1}</p></div>
+        <div className="d-flex"><p className="mx-2"> {props.description2}</p><p> {props.result2}</p></div>
+        <div className="d-flex"><p className="mx-2"> {props.description3}</p><p> {props.result3}</p></div>
+        
+        
+        <div className="d-flex justify-content-between">
+          <Link to={`/detail/${props.path}/${props.index}`}>
+          <button  className="btn btn-outline-primary">Learn more!</button>
+          </Link>
+          <button onClick={() => actions.setFavorites(props.title)} className=" btn btn-outline-warning">
+            <i className="far fa-heart" />
+          </button>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+Card.propTypes={
+  title: propTypes.string,
+  description1: propTypes.string,
+  result1: propTypes.string,
+  description2: propTypes.string,
+  result2: propTypes.string,
+  description3: propTypes.string,
+  result3: propTypes.string,
+  path: propTypes.string,
+  index: propTypes.number,
+  img: propTypes.string
+  
+}
+
+
--- src/js/component/navbar.js
+++ src/js/component/navbar.js
@@ -1,17 +1,46 @@
-import React from "react";
+import React, { useEffect, useContext } from "react";
+import { Context } from "../store/appContext";
 import { Link } from "react-router-dom";
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTrash } from '@fortawesome/free-solid-svg-icons'
 
 export const Navbar = () => {
-	return (
-		<nav className="navbar navbar-light bg-light mb-3">
-			<Link to="/">
-				<span className="navbar-brand mb-0 h1">React Boilerplate</span>
-			</Link>
-			<div className="ml-auto">
-				<Link to="/demo">
-					<button className="btn btn-primary">Check the Context in action</button>
-				</Link>
-			</div>
-		</nav>
-	);
+	const { store, actions } = useContext(Context);
+
+	const style = {
+		width: "100px",
+
+	  };
+
+	  
+	
+  return (
+    <nav className="navbar navbar-light bg-light mb-3 px-5">
+      <Link to="/">
+        <img style={style} src="https://1000marcas.net/wp-content/uploads/2019/12/Star-Wars-Logo-5.png"></img>
+      </Link>
+      <div className="ml-auto">
+        <div className="dropdown">
+          <button
+            className="btn btn-primary dropdown-toggle"
+            type="button"
+            id="dropdownMenuButton1"
+            data-bs-toggle="dropdown"
+			
+            aria-expanded="false"
+          >
+            Favorites {store.favList.length}
+          </button>
+          <ul className="dropdown-menu" aria-labelledby="dropdownMenuButton1">
+
+		  {store.favList.map((item, index) => {
+          return (<li className="dropdown-item" key={index}>{item} <FontAwesomeIcon onClick={()=>{actions.deleteFavorites(index)}} icon={faTrash} /></li>)
+
+        })}
+           
+          </ul>
+        </div>
+      </div>
+    </nav>
+  );
 };
